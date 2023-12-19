693eb51a35f2007f8724f19b2a5c734f448a076d
Salisatoru <95071118+Salisatoru@users.noreply.github.com>
2023-12-19 15:24:06 +0700

    Update lab9_1.cpp

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -3,9 +3,13 @@
 using namespace std;
 
 int main(){	
+	double NewBalance, rpy, pyp;
 	cout << "Enter initial loan: ";
+	cin >> NewBalance;
 	cout << "Enter interest rate per year (%): ";
+	cin >> rpy;
 	cout << "Enter amount you can pay per year: ";
+	cin >> pyp;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -18,16 +22,33 @@ int main(){
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
 	
+	int i=1;
+	double Interest, Total;
+	do{
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+		cout << fixed << setprecision(2); 
+		cout << setw(13) << left << i;
+		cout << setw(13) << left << NewBalance;
+		Interest=NewBalance*rpy/100;
+		cout << setw(13) << left << Interest;
+		Total=NewBalance+Interest;
+		cout << setw(13) << left << Total;
+		if(Total>=pyp){
+			cout << setw(13) << left << pyp;
+		}else{
+			cout << setw(13) << left << Total;
+		}
+		if(Total>=pyp){
+			NewBalance=Total-pyp;
+		}else{
+			NewBalance=Total-Total;
+		}
+		cout << setw(13) << left << NewBalance;
+		cout << "\n";	
+		if(NewBalance==0) break;
+		i++;
+	}while (true) ;
 	
 	return 0;
 }
